2828396564861b343da22fab277af0e366c280dd
Matthew de Detrich <matthew.de.detrich@zalando.de>
2019-06-05 15:11:33 +0200

    Apply new scalafmt
    Signed-off-by: Matthew de Detrich <matthew.de.detrich@zalando.de>
    
    (cherry picked from commit 00d920dd5f162889a588554dac33e4bbc4f9a646)

--- src/main/scala/org/zalando/kanadi/models/CustomHeaders.scala
+++ src/main/scala/org/zalando/kanadi/models/CustomHeaders.scala
@@ -3,7 +3,6 @@ package org.zalando.kanadi.models
 import akka.http.scaladsl.model.headers.RawHeader
 
 final case class CustomHeaders(headers: Map[String, String]) extends AnyVal {
-  def toRawHeaders: List[RawHeader] = {
+  def toRawHeaders: List[RawHeader] =
     headers.to[List].map { case (k, v) => RawHeader(k, v) }
-  }
 }
