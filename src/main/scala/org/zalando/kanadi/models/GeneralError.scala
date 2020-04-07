c99b26bae4fdbc69511b6b5caaef419587deb4fd
Matthew de Detrich <mdedetrich@gmail.com>
2020-04-07 15:04:15 +0200

    Marshal the response body into HttpServiceError
    Signed-off-by: Matthew de Detrich <mdedetrich@gmail.com>

--- src/main/scala/org/zalando/kanadi/models/GeneralError.scala
+++ src/main/scala/org/zalando/kanadi/models/GeneralError.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import org.mdedetrich.webmodels.Problem
 
 class GeneralError(val problem: Problem, override val httpRequest: HttpRequest, override val httpResponse: HttpResponse)
-    extends HttpServiceError(httpRequest, httpResponse) {
+    extends HttpServiceError(httpRequest, httpResponse, Right(problem)) {
   override def getMessage: String = s"Error from server, response is $problem"
 }
 
@@ -15,7 +15,7 @@ final case class OtherError(error: BasicServerError) extends Exception {
 class ExpectedHeader(val headerName: String,
                      override val httpRequest: HttpRequest,
                      override val httpResponse: HttpResponse)
-    extends HttpServiceError(httpRequest, httpResponse) {
+    extends HttpServiceError(httpRequest, httpResponse, Left("")) {
   override def getMessage: String =
     s"Expected header with name: $headerName, request is $httpRequest, response is $httpResponse"
 }
--- src/main/scala/org/zalando/kanadi/models/HttpServiceError.scala
+++ src/main/scala/org/zalando/kanadi/models/HttpServiceError.scala
@@ -1,5 +1,9 @@
 package org.zalando.kanadi.models
 
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import org.mdedetrich.webmodels.Problem
 
-class HttpServiceError(val httpRequest: HttpRequest, val httpResponse: HttpResponse) extends Exception
+class HttpServiceError(val httpRequest: HttpRequest,
+                       val httpResponse: HttpResponse,
+                       val stringOrProblem: Either[String, Problem])
+    extends Exception
