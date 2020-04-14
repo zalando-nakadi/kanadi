866bdaaaaf81369ad1fadc3d9e08f5f0e9b13363
Matthew de Detrich <mdedetrich@gmail.com>
2020-04-14 11:41:49 +0200

    Improve error message for HttpServiceError
    Signed-off-by: Matthew de Detrich <mdedetrich@gmail.com>
    (cherry picked from commit a65096c0d5bac37d1fbc8812ad008db1b42898c2)

--- src/main/scala/org/zalando/kanadi/models/HttpServiceError.scala
+++ src/main/scala/org/zalando/kanadi/models/HttpServiceError.scala
@@ -6,4 +6,8 @@ import org.mdedetrich.webmodels.Problem
 class HttpServiceError(val httpRequest: HttpRequest,
                        val httpResponse: HttpResponse,
                        val stringOrProblem: Either[String, Problem])
-    extends Exception
+    extends Exception {
+  override def getMessage: String = s"General error in server response, $toString"
+  override def toString =
+    s"Response content is $stringOrProblem, Request is $httpRequest, Response is ${httpResponse.toString()}"
+}
